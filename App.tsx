0e2a43e795920c920fa0431d571d32e74e551850
Meena Bassem <mbassem@elements-ft.com>
2020-12-28 14:51:06 +0200

    - Add react-native-kashier-1.1.0.tgz

--- App.tsx
+++ App.tsx
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View ,Button} from 'react-native';
+import {Kashier} from 'react-native-kashier';
+import { Card } from 'react-native-kashier/src/Model/Card';
 
 export default function App() {
+  useEffect(()=>{
+    const ApiKey = '********-****-****-****-************';
+    const merchantId = 'MID-XXXX-XXXX';
+    
+    
+    Kashier.initialize({
+        merchantId: merchantId,
+        apiKey: ApiKey,
+        currency: 'EGP',
+        sdkMode: 'DEVELOPMENT',
+        displayLang: 'EN',
+      });
+  },[])
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <Button title={"Test"} onPress={()=>{
         console.log("Button Pressed")
+        Kashier.saveCard(
+          Card.fromExpiryMonth_Year(
+            'Kashier Test Name',
+            '5111111111111118',
+            '100',
+            '05',
+            '21',
+          ),
+          "shopperRef",
+          'perm',
+          result => {
+            console.log('Token Created Successfully', result);
+          },
+          error => {
+            console.log('Error', error.errorType, error.errorMessage);
+          },
+        )
       }}/>
     </View>
   );
